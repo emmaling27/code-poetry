59df340bbbcca17c2c2b33354f73c25983e4666c
Emma Ling <emmaling27@gmail.com>
2020-04-30 12:49:19 -0700

    colors of boxes change to random rainbow colors

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ const col = grid[0];
 const row = grid[1];
 const numberOfElements = col * row;
 
+const red = '#FF6663';
+const orange = '#FEB144';
+const yellow = '#FDFD97';
+const green = '#9EE09E';
+const blue = '#9EC1CF';
+const purple = '#CC99C9';
+const colors = [red, orange, yellow, green, blue, purple];
+
+let box_colors = []
 for (let i = 0; i < numberOfElements; i++) {
-  fragment.appendChild(document.createElement('div'));
+  const elt = document.createElement('div');
+  elt.setAttribute("id", i);
+  fragment.appendChild(elt);
+  box_colors.push(_.sample(colors))
 }
 
 staggerVisualizerEl.appendChild(fragment);
@@ -19,6 +31,9 @@ const staggersAnimation = anime.timeline({
   autoplay: false
 })
 .add({
+  backgroundColor: function(el, i, l) {
+    return box_colors[i]
+  },
   translateX: [
     {value: anime.stagger('-.1rem', {grid: grid, from: 'center', axis: 'x'}) },
     {value: anime.stagger('.1rem', {grid: grid, from: 'center', axis: 'x'}) }
@@ -36,31 +51,31 @@ const staggersAnimation = anime.timeline({
   translateY: () => anime.random(-10, 10),
   delay: anime.stagger(8, {from: 'last'})
 })
-// .add({
-//   translateX: anime.stagger('.25rem', {grid: grid, from: 'center', axis: 'x'}),
-//   translateY: anime.stagger('.25rem', {grid: grid, from: 'center', axis: 'y'}),
-//   rotate: 0,
-//   scaleX: 2.5,
-//   scaleY: .25,
-//   delay: anime.stagger(4, {from: 'center'})
-// })
-// .add({
-//   rotate: anime.stagger([90, 0], {grid: grid, from: 'center'}),
-//   delay: anime.stagger(50, {grid: grid, from: 'center'})
-// })
-// .add({
-//   translateX: 0,
-//   translateY: 0,
-//   scale: .5,
-//   scaleX: 1,
-//   rotate: 180,
-//   duration: 1000,
-//   delay: anime.stagger(100, {grid: grid, from: 'center'})
-// })
-// .add({
-//   scaleY: 1,
-//   scale: 1,
-//   delay: anime.stagger(20, {grid: grid, from: 'center'})
-// })
+.add({
+  translateX: anime.stagger('.25rem', {grid: grid, from: 'center', axis: 'x'}),
+  translateY: anime.stagger('.25rem', {grid: grid, from: 'center', axis: 'y'}),
+  rotate: 0,
+  scaleX: 2.5,
+  scaleY: .25,
+  delay: anime.stagger(4, {from: 'center'})
+})
+.add({
+  rotate: anime.stagger([90, 0], {grid: grid, from: 'center'}),
+  delay: anime.stagger(50, {grid: grid, from: 'center'})
+})
+.add({
+  translateX: 0,
+  translateY: 0,
+  scale: .5,
+  scaleX: 1,
+  rotate: 180,
+  duration: 1000,
+  delay: anime.stagger(100, {grid: grid, from: 'center'})
+})
+.add({
+  scaleY: 1,
+  scale: 1,
+  delay: anime.stagger(20, {grid: grid, from: 'center'})
+})
 
 staggersAnimation.play();
